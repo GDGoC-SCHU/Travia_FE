1fddaaa51d3d40ea4b5aeaf02881a80e8b4ca0ec
paperbox <7126454+lego37yoon@users.noreply.github.com>
2025-05-06 22:55:47 +0900

    feat: add link to step 3

--- app/routes/step2.tsx
+++ app/routes/step2.tsx
@@ -1,15 +1,27 @@
-import { createFileRoute } from '@tanstack/react-router'
+import CardSection from '@/components/CardSection';
+import Title from '@/components/Title';
+import { Button, buttonVariants } from '@/components/ui/button';
+import { createFileRoute, Link, redirect, useNavigate, useRouter } from '@tanstack/react-router'
 import { createServerFn } from '@tanstack/react-start';
+import { setCookie } from '@tanstack/react-start/server';
+import { Blend, CircleArrowLeft, Handshake, Heart, User } from 'lucide-react';
 
 const getName = createServerFn({
   method: "GET",
 })
 .validator((data: string) => data)
 .handler(async (ctx) => {
+  setCookie("name", ctx.data);
+
   return {
     name: ctx.data
   }
-})
+});
+
+const selectType = (type: string) => {
+  localStorage.setItem("with", type);
+}
+
 
 export const Route = createFileRoute('/step2')({
   component: SecondStep,
@@ -27,6 +39,48 @@ export const Route = createFileRoute('/step2')({
 
 function SecondStep() {
   const data = Route.useLoaderData();
+  const router = useRouter();
 
-  return <p>안녕하세요, {data.name}님!</p>
+  return (
+    <>
+    <Title type="h1" text="누구와 함께 하실 계획이신가요?" name={data.name} />
+    <CardSection>
+      <form className="w-full lg:w-64 grid grid-cols-2 lg:flex gap-2 lg:flex-nowrap! flex-wrap" onSubmit={async (e) => {
+        e.preventDefault();
+        const type = localStorage.getItem("with");
+
+        if (type) {
+          localStorage.clear();
+          router.navigate({
+            viewTransition: true,
+            href: `/step3?type=${type}`
+          });
+        }
+      }}>
+        <Button type="submit" variant={"secondary"} className="text-xl flex-col h-32 lg:w-32" onClick={(e) => selectType("single")}>
+          <User size={48} className="grow min-w-8" strokeWidth={1} />
+          혼자
+        </Button>
+        <Button type="submit" variant={"secondary"} className="text-xl flex-col h-32 lg:w-32" onClick={(e) => selectType("friend")}>
+          <Handshake size={48} className="grow min-w-8" strokeWidth={1} />
+          친구와
+        </Button>
+        <Button type="submit" variant={"secondary"} className="text-xl flex-col h-32 lg:w-32" onClick={(e) => selectType("sweetheart")}>
+          <Heart size={48} className="grow min-w-8" strokeWidth={1} />
+          연인과
+        </Button>
+        <Button type="submit" variant={"secondary"} className="text-xl flex-col h-32 lg:w-32" onClick={(e) => selectType("family")}>
+          <Blend size={48} className="grow min-w-8" strokeWidth={1} />
+          가족과
+        </Button>
+      </form>
+      <div className="w-fit">
+        <Link to="/" className={ buttonVariants() }>
+          <CircleArrowLeft />
+          이전
+        </Link>
+      </div>
+    </CardSection>
+    </>
+  )
 }
