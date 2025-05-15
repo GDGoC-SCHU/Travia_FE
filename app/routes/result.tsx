48ac56b2d40f0ef8e421a0525b84df9a0aaa6861
paperbox <7126454+lego37yoon@users.noreply.github.com>
2025-05-15 21:49:11 +0900

    feat: lazy loading for data

--- app/routes/result.tsx
+++ app/routes/result.tsx
@@ -3,7 +3,7 @@ import { Accordion, AccordionItem, AccordionTrigger } from '@/components/ui/acco
 import { Button, buttonVariants } from '@/components/ui/button';
 import { Card } from '@/components/ui/card';
 import { AccordionContent } from '@radix-ui/react-accordion';
-import { createFileRoute, Link, redirect } from '@tanstack/react-router'
+import { createFileRoute, Link, redirect, defer, Await, ErrorComponent } from '@tanstack/react-router'
 import { createServerFn } from '@tanstack/react-start';
 import { getCookie } from '@tanstack/react-start/server';
 import { CircleArrowLeft, CircleArrowRight, Coffee, CakeSlice, Pizza, Pyramid, FishSymbol, CircleEllipsis, FlameKindling, Leaf, Snowflake, Footprints, Armchair, Activity, TriangleAlert, ArrowLeftCircle, ArrowRightCircle, Home, LoaderCircle } from 'lucide-react';
@@ -41,6 +41,13 @@ const getResult = createServerFn({
   } else if (!data) {
     throw redirect({ to: "/step5", search: { schedule: schedule, budget: budget, transport: transport }})
   } else {
+    titleInfo.setState((state) => {
+      return {
+      ...state,
+      type: "h1",
+      text: "Recommends these cities based on your information.",
+    }});
+
     return {
       name: name,
       travelWith: travelWith,
@@ -54,46 +61,8 @@ const getResult = createServerFn({
     }
   }
 })
-.handler(async (ctx) => {
-  const resultResponse = await fetch(`${import.meta.env.VITE_BACKEND_ADDRESS}/api/v1/survey/recommend`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "accept": "application/json"
-    },
-    body: JSON.stringify({
-      "username": ctx.data.name,
-      "preferences": {
-        "companion": ctx.data.travelWith,
-        "style": Array.isArray(ctx.data.actType) ? ctx.data.actType : [ctx.data.actType], // ✅ 여기를 배열로 보장
-        "duration": ctx.data.schedule,
-        "budget": ctx.data.budget,
-        "climate": ctx.data.env,
-        "continent": ctx.data.cont,
-        "density": ctx.data.pace,
-        "driving": ctx.data.transport
-      }
-    })
-  });
-
-  if (resultResponse.ok) {
-    return {
-      name: ctx.data.name,
-      schedule: ctx.data.schedule,
-      budget: ctx.data.budget,
-      transport: ctx.data.transport,
-      result: await resultResponse.json()
-    }
-  } else {
-    //throw new Error(resultResponse.status.toString());
-    return {
-      name: ctx.data.name,
-      schedule: ctx.data.schedule,
-      budget: ctx.data.budget,
-      transport: ctx.data.transport,
-      error: resultResponse.status
-    }
-  }
+.handler((ctx) => {
+  return ctx.data;
 });
 
 export const Route = createFileRoute('/result')({
@@ -107,29 +76,40 @@ export const Route = createFileRoute('/result')({
   loaderDeps: ({ search: { cont, env, pace } }) => ({
     cont, env, pace
   }),
-  loader: async ({ deps: { cont, env, pace } }) => (
-    await getResult({ data: {
+  loader: async ({ deps: { cont, env, pace } }) => {
+    const data = await getResult({ data: {
       cont: cont,
       env: env,
       pace: pace }
-    })
-  )
-})
+    });
+    
+    const resultResponse = fetch(`${import.meta.env.VITE_BACKEND_ADDRESS}/api/v1/survey/recommend`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "accept": "application/json"
+      },
+      body: JSON.stringify({
+        "username": data.name,
+        "preferences": {
+          "companion": data.travelWith,
+          "style": data.actType,
+          "duration": data.schedule,
+          "budget": data.budget,
+          "climate": data.env,
+          "continent": data.cont,
+          "density": data.pace,
+          "driving": data.transport
+        }
+      })
+    }).then(async (data) => await data.json());
 
-async function RouteComponent() {
-  const data = Route.useLoaderData();
-  const [index, setIndex] = useState<number>(0);
-
-  titleInfo.setState((state) => {
     return {
-    ...state,
-    type: "h1",
-    text: "Recommends these cities based on your information.",
-  }});
-
-  return (
-    <CardSection>
-    {data.error || !data.result.data ? (
+      data, resultResponse
+    }
+  },
+  errorComponent: ({ error }) => {
+    return (
       <div className="text-center flex flex-col justify-center items-center">
       <h2 className="text-2xl">
         <TriangleAlert size={48} className="grow min-w-12" strokeWidth={1} />
@@ -139,75 +119,106 @@ async function RouteComponent() {
         Try again later. If you encounter this error frequently,
         <a href="https://github.com/GDGoC-SCHU/Travia_FE/issues">file a bug to the issue tracker</a>.
       </p>
+      <p>{error.message}</p>
       </div>
-    ): (
-      <>
-      <div className="flex justify-between lg:justify-start gap-4 items-center">
-        <Button
-          disabled={index === 0}
-          onClick={(e) => setIndex(index - 1)}>
-          <ArrowLeftCircle size={48} />
-        </Button>
-        <span className="text-xl">{index + 1} / {data.result.data.length}</span>
-        <Button
-          disabled={index === data.result.data.length - 1}
-          onClick={(e) => setIndex(index + 1)}>
-          <ArrowRightCircle size={48} />
-        </Button>
-      </div>
-      <div className="flex">
-        <div className="pt-4 grow shrink min-w-0">
-          <h2 className="text-2xl">
-            <span className="text-base block">
-              {data.result.data[index].country}
-            </span>
-            {data.result.data[index].city}
-          </h2>
-          <Accordion type="single" collapsible className="lg:me-4">
-            <AccordionItem value="reason">
-              <AccordionTrigger className="my-2 text-xl hover:bg-gray-50 px-4 hover:no-underline">📝 Reasons to recommend!</AccordionTrigger>
-              <AccordionContent>
-                <motion.p className="p-4 bg-blue-50 rounded-md"
-                  initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
-                  {data.result.data[index].reason}
-                </motion.p>
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
+    )
+  }
+})
 
-          <div className="max-w-(--scrollable-max-width) overflow-x-scroll flex gap-2 py-8 px-4 rounded-2xl">
-          {Object.keys(data.result.data[index].schedule).map((day, idx) => (
-            <Card className="shadow-xl p-4 shrink-0 w-6/7 lg:w-72" key={idx}>
-              <h3 className="text-xl">Day {day.split("_")[1]}</h3>
-              {data.result.data[index].schedule[day].map((ev : { time: string, activity: string }, idx: number) => (
-                <p key={idx}>
-                  <span className="text-lg block">⌚ {ev.time}</span>
-                  <span>{ev.activity}</span>
-                </p>
-              ))}
-            </Card>
-          ))}
+async function RouteComponent() {
+  const { data, resultResponse } = Route.useLoaderData();
+  const [index, setIndex] = useState<number>(0);
+
+  return (
+    <Await promise={resultResponse} fallback={
+      <section className="w-screen h-full z-30 backdrop-blur-md">
+        <LoaderCircle className="accent-cyan-600" />
+        We're making recommendations for you...
+      </section>
+    }>
+      {(result) => (
+        <CardSection>
+        {result.data ? (
+          <>
+          <div className="flex justify-between lg:justify-start gap-4 items-center">
+            <Button
+              disabled={index === 0}
+              onClick={() => setIndex(index - 1)}>
+              <ArrowLeftCircle size={48} />
+            </Button>
+            <span className="text-xl">{index + 1} / {result.data.length}</span>
+            <Button
+              disabled={index === result.data.length - 1}
+              onClick={() => setIndex(index + 1)}>
+              <ArrowRightCircle size={48} />
+            </Button>
           </div>
-        </div>
-        <div className="hidden lg:block py-4 shrink-0">
-          <motion.img initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 0.5 }}
-            src={`https://maps.googleapis.com/maps/api/staticmap?center=${data.result.data[index].country}+${data.result.data[index].city}&zoom=12&size=250x250&key=${import.meta.env.VITE_GOOGLE_API_KEY}`}
-            alt={`${data.result.data[index].city} 지도`} className="rounded-xl aspect-square" />
-        </div>
-      </div>
-      </>
-    )}
-    <div className="flex justify-between lg:justify-start gap-4 items-center">
-      <Link to={`/step5`} className={ buttonVariants() }
-        search={{ schedule: data.schedule, budget: data.budget, transport: data.transport }}>
-        <CircleArrowLeft />
-        Back
-      </Link>
-      <Link to={`/`} className={ buttonVariants() }>
-        <Home />
-        Go to the first page
-      </Link>
-    </div>
-    </CardSection>
+          <div className="flex">
+            <div className="pt-4 grow shrink min-w-0">
+              <h2 className="text-2xl">
+                <span className="text-base block">
+                  {result.data[index].country}
+                </span>
+                {result.data[index].city}
+              </h2>
+              <Accordion type="single" collapsible className="lg:me-4">
+                <AccordionItem value="reason">
+                  <AccordionTrigger className="my-2 text-xl hover:bg-gray-50 px-4 hover:no-underline">📝 Reasons to recommend!</AccordionTrigger>
+                  <AccordionContent>
+                    <motion.p className="p-4 bg-blue-50 rounded-md"
+                      initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
+                      {result.data[index].reason}
+                    </motion.p>
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+
+              <div className="max-w-(--scrollable-max-width) overflow-x-scroll flex gap-2 py-8 px-4 rounded-2xl">
+                {Object.keys(result.data[index].schedule).map((day, idx) => (
+                  <Card className="shadow-xl p-4 shrink-0 w-6/7 lg:w-72" key={idx}>
+                    <h3 className="text-xl">Day {day.split("_")[1]}</h3>
+                    {result.data[index].schedule[day].map((ev : { time: string, activity: string }, idx: number) => (
+                      <p key={idx}>
+                        <span className="text-lg block">⌚ {ev.time}</span>
+                        <span>{ev.activity}</span>
+                      </p>
+                    ))}
+                  </Card>
+                ))}
+              </div>
+            </div>
+            <div className="hidden lg:block py-4 shrink-0">
+              <motion.img initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 0.5 }}
+                src={`https://maps.googleapis.com/maps/api/staticmap?center=${result.data[index].country}+${result.data[index].city}&zoom=12&size=250x250&key=${import.meta.env.VITE_GOOGLE_API_KEY}`}
+                alt={`${result.data[index].city} 지도`} className="rounded-xl aspect-square" />
+            </div>
+          </div>
+          </>
+        ): (
+          <div className="text-center flex flex-col justify-center items-center">
+            <h2 className="text-2xl">
+              <TriangleAlert size={48} className="grow min-w-12" strokeWidth={1} />
+              Server Error occured
+            </h2>
+            <p>
+              Try again later. If you encounter this error frequently,
+              <a href="https://github.com/GDGoC-SCHU/Travia_FE/issues">file a bug to the issue tracker</a>.
+            </p>
+          </div>
+        )}
+        <div className="flex justify-between lg:justify-start gap-4 items-center">
+            <Link to={`/step5`} className={ buttonVariants() }
+              search={{ schedule: data.schedule, budget: data.budget, transport: data.transport }}>
+              <CircleArrowLeft />
+              Back
+            </Link>
+            <Link to={`/`} className={ buttonVariants() }>
+              <Home />
+              Go to the first page
+            </Link>
+          </div>          
+        </CardSection>
+      )}
+    </Await>
   )
 }
